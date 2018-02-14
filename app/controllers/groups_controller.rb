a32d7a1f20da37c65d09b567d7112ca33bc47ce2
新田　誠 <lacampanella0522@icloud.com>
2018-02-14 13:40:53 +0900

    chat_group_create.end

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,8 +3,16 @@ class GroupsController < ApplicationController
     @group = Group.new
     @group.users << current_user
   end
+  def index
+  end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -13,4 +21,9 @@ class GroupsController < ApplicationController
   def update
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
 end
+
