13d489a0aa54629c4796b9a0269b6ce69891aeef
新田　誠 <lacampanella0522@icloud.com>
2018-02-26 14:48:36 +0900

    automatic_update.close2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,51 +1,20 @@
-$(function(){
+$(function() {
   function buildHTML(message) {
-    if (message.content == null)
-      return
-    var image = (message.image == null) ? "" : `<img src="${message.image}">`;
-    var html = `<div class="message" data-id="${message.id}">
-                  <div class="upper-message">
-                    <div class="upper-message__user-name">${message.user_name}</div>
-                    <div class="upper-message__date">${message.date}</div>
+    var image = (message.image !== null) ? `<img src = "${message.image}">`:''
+    var html = `<div class = "message" data-id = "${message.id}">
+                  <div class = "upper-message">
+                    <p class = "upper-message__user-name">${message.user_name}</p>
+                    <p class = "upper-message__date">${message.date}</p>
                   </div>
-                  <div class="lower-meesage">
-                    <p class="lower-meesage_content">${message.content}
-                  </div>
-                  <div class="lower-message__image">
-                    ${image}
-                  </div>
+                     <p class="lower-meesage_content">${message.content}</p>
+                     <p class="lower-message__image">${image}</p>
                 </div>`
     return html;
-  };
-  setInterval(function() {
-  autoUpdate();
-  }, 2000);
-  function autoUpdate(){
-    var message_id = $(".message").last().data("id");
-    if (!window.location.href.match(/\/groups\/\d+\/messages/)) {
-      return
-    };
-    $.ajax({
-      url: location.href,
-      type: "GET",
-      data: { id: message_id },
-      dataType: "json"
-    })
-    .done(function(data) {
-      data.forEach(function(data) {
-        var html = buildHTML(data);
-        $(".messages").append(html)
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-      })
-    })
-    .fail(function() {
-      alert("エラーが発生しました。")
-    })
-  };
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
+  }
+ $('#new_message').on('submit', function(e) {
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
@@ -53,18 +22,37 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-     })
-    .done(function(data){
-      console.log(data);
+    })
+    .done(function(data) {
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.form__message').val('')
       $('.hidden').val('')
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-     })
-    .fail(function(){
-      alert('エラーが発生しました。');
-      pageRESET()
+      moveToBottom()
+    })
+    .fail(function() {
+      alert("エラーが発生しました。");
     })
   })
-});
+  function autoUpdate(){
+    var message_id = $(".message").last().data("id");
+    $.ajax({
+      url: location.href,
+      type: "GET",
+      data: { id: message_id },
+      dataType: "json"
+    })
+    .done(function(data) {
+      data.forEach(function(dat) {
+      var html = buildHTML(dat);
+      $('.messages').append(html);
+      moveToBottom()
+      })
+    })
+  }
+  setInterval(autoUpdate, 2000);
+
+  function moveToBottom() {
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+  }
+})
